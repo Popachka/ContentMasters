1aa26f239e0147ccbeca7ec0d4f3c5c2514e04ff
Popachka <swarm21s@mail.ru>
2024-10-01 15:55:21 +0300

    new deploy

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Создайте экземпляр Axios с базовым URL
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8888/api/v1/'
+  baseURL: 'https://spichkintut.ru/api/v1/'
 });
 
 // Добавьте обработчик запроса для установки заголовков авторизации
